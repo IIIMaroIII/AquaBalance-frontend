6c0e2ce59b166a8095899be36130c0dcb76ebd73
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-08-03 02:10:44 -0600

    upd selectors added memo selector

--- src/redux/users/selectors.js
+++ src/redux/users/selectors.js
@@ -1,4 +1,5 @@
 export const selectUser = state => state.users.user;
+export const selectDailyNorma = state => state.users.user.dailyNorma;
 export const selectUserToken = state => state.users.user.token;
 export const selectUserIsLoggedIn = state => state.users.isLoggedIn;
 export const selectRefreshing = state => state.users.isRefreshing;
--- src/redux/water/selectors.js
+++ src/redux/water/selectors.js
@@ -1,3 +1,6 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { selectDailyNorma } from "../users/selectors.js";
+
 export const selectWaterItems = state => state.water.water.dailyItems;
 export const selectMonthlyWaterItems = state => state.water.water.monthlyItems;
 
@@ -20,25 +23,25 @@ export const selectIsWaterModalEdit = state =>
 export const selectIsWaterModalAdd = state =>
   state.water.modalFlags.isWaterModalAdd;
 
-export const selectPercentage = state => {
-  const waterItems = selectWaterItems(state);
-  const dailyNorma = state.users.user.dailyNorma;
-
-  const volume = () => {
-    if (waterItems !== null) {
-      return waterItems
-        .map(item => item.volume)
-        .reduce((acc, volume) => {
-          return acc + volume;
+export const selectPercentage = createSelector(
+  [selectWaterItems, selectDailyNorma],
+  (waterItems, dailyNorma) => {
+    const volume = () => {
+      if (waterItems !== null) {
+        return waterItems
+          .map(item => item.volume)
+          .reduce((acc, volume) => {
+            return acc + volume;
         }, 0);
-    }
-  };
+      }
+    };
 
-  const percentage = (volume() / (dailyNorma * 1000)) * 100;
-  if (isNaN(percentage)) {
-    return 0;
-  } else {
-    if (percentage < 100) return percentage;
-    if (percentage > 100) return 100;
-  }
-};
+    const percentage = (volume() / (dailyNorma * 1000)) * 100;
+    if (isNaN(percentage)) {
+      return 0;
+    } else {
+      if (percentage < 100) return percentage;
+      if (percentage > 100) return 100;
+    }
+  },
+);
