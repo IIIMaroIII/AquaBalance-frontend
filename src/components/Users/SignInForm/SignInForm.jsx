d58179517fc40a050b27d8c1c3973eb5ef3a5d26
Anton <antonklim6@gmail.com>
2024-08-05 22:23:49 +0200

    upd a little

--- src/components/Users/SignInForm/SignInForm.jsx
+++ src/components/Users/SignInForm/SignInForm.jsx
@@ -2,6 +2,7 @@
 import Button from 'src/components/REUSABLE/Button/Button';
 import CustomInput from 'src/components/REUSABLE/Input/CustomInput';
 // import { signIn } from 'src/redux/users/operations';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useId } from "react";
 import { useDispatch } from "react-redux";
@@ -12,30 +13,36 @@ const SignInForm = () => {
   const passwordId = useId();
   const dispatch = useDispatch();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const { register, handleSubmit, reset, formState: { errors }, } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
   }
-
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <p>Sign in</p>
-      <label htmlFor={emailId}>Email</label>
       <CustomInput
         label={true}
+        labelName={"Email"}
         inputType={"text"}
         placeholder={"Enter your email"}
         name={"email"}
         id={emailId}
+        error={errors.email ? true : false}
+        {...register("email", { required: true })}
       />
-      <label htmlFor={passwordId}>Password</label>
+      {errors.email && <span>{errors.email.message}</span>}
+
       <CustomInput
         label={true}
+        labelName={"Password"}
         inputType={"password"}
         placeholder={"Enter your password"}
         name={"password"}
         id={passwordId}
+        {...register("password", { required: true })}
       />
-
+      {errors.password && <span>{errors.password.message}</span>}
       <Button type="submit" value="submit">Sign In</Button>
     </form>
 )
