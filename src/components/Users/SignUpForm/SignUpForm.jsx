7c07df926ee2606fa4ea646c09606b2b93025fdb
Anton <antonklim6@gmail.com>
2024-08-05 17:07:59 +0200

    SingUp component, Форма без логікі

--- src/components/Users/SignUpForm/SignUpForm.jsx
+++ src/components/Users/SignUpForm/SignUpForm.jsx
@@ -1,10 +1,41 @@
 import css from './signUpForm.module.css';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useId } from "react";
+
 import { useState } from 'react';
 import Button from 'src/components/REUSABLE/Button/Button'; // Переиспользуемый кастомный компонент Button - активно узать вместо тэга
-import Logo from 'src/components/REUSABLE/Logo/Logo';
 
 const SignUpForm = () => {
-  return <></>;
+  const emailId = useId();
+  const passwordId = useId();
+  const rpasswordId = useId();
+
+  return (
+      <Formik initialValues={{
+          email: "",
+          password: "",
+          rpassword: ""
+        }} onSubmit={(values, actions) => {
+          console.log(values);
+          actions.resetForm();
+        }}
+        >
+        <Form>
+          <h2>Sign Up</h2>
+          <label htmlFor={emailId}>Email</label>
+          <Field type="text" name="email" placeholder="Enter your email" id={emailId}/>
+
+          <label htmlFor={passwordId}>Password</label>
+          <Field type="password" name="password" placeholder="Enter your password" id={passwordId}/>
+
+          <label htmlFor={rpasswordId}>Repeat password</label>
+          <Field type="password" name="rpassword" placeholder="Repeat password" id={rpasswordId}/>
+
+          <Button type="submit">Sign Up</Button>
+        </Form>
+      </Formik>
+    
+)
 };
 
 export default SignUpForm;
--- src/pages/SignUp/SignUpPage.jsx
+++ src/pages/SignUp/SignUpPage.jsx
@@ -1,13 +1,21 @@
 import SignUpForm from 'src/components/Users/SignUpForm/SignUpForm.jsx';
 // import css from './SignUpPage.module.css';
 import Container from 'src/components/REUSABLE/Container/Container';
+import Logo from 'src/components/REUSABLE/Logo/Logo';
+import { Link } from 'react-router-dom';
 
 const SignUpPage = () => {
   // const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
   return (
     <Container type="section" addClass={'wrapper'}>
+      <Logo />
       <SignUpForm />
+      <div>
+        <p>
+          Already have an account? <Link to="/signin">Sign In</Link>
+        </p>
+      </div>
     </Container>
   );
 };
