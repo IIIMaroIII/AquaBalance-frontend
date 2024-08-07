ccb1fe205c6890ad9fc85e5bbc4dd2cfb8a32f18
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-08-07 19:42:28 +0300

    added hidePass logic

--- src/components/Users/SignUpForm/SignUpForm.jsx
+++ src/components/Users/SignUpForm/SignUpForm.jsx
@@ -1,17 +1,24 @@
-import css from './signUpForm.module.css';
+// import css from './signUpForm.module.css';
 import { useForm } from 'react-hook-form';
 import { signUp } from 'src/redux/users/operations';
 import { yupResolver } from "@hookform/resolvers/yup";
 import { signUpFormValidation } from 'src/Validation/signUpFormValidation';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Button from 'src/components/REUSABLE/Button/Button';
 import CustomInput from 'src/components/REUSABLE/Input/CustomInput';
-
+import eye from '../../../assets/temporarySVG/eye.svg'
+import eyeOff from '../../../assets/temporarySVG/eye-off.svg'
+import { useState } from 'react';
 const SignUpForm = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-
+  // const navigate = useNavigate();
+  const [hidePass, setHidePass] = useState(true)
+  console.log(hidePass);
+const toggleHidePass = () => {
+  setHidePass(prevState => !prevState)
+  console.log(hidePass);
+}
   const { register, handleSubmit, reset, formState: { errors, isDirty, isValid}, } = useForm({
     resolver: yupResolver(signUpFormValidation),
   });
@@ -47,14 +54,22 @@ const SignUpForm = () => {
         />
         {errors.password && <span>{errors.password.message}</span>}
         <CustomInput
-          label={true}
-          labelName={"Repeat password"}
-          inputType={"password"}
-          placeholder={"Repeat password"}
-          name={"repeatPassword"}
-          error={errors.password ? true : false}
-          {...register("repeatPassword")}
-        />
+        label={true}
+        labelName={"Repeat password"}
+        inputType={"password"}
+        placeholder={"Repeat password"}
+        name={"repeatPassword"}
+        error={errors.password ? true : false}
+        type={hidePass ? 'password' : 'text'}
+        {...register("repeatPassword")}
+        >
+        <Button
+disabled={!isDirty || !isValid}
+        type="button"
+         onClick={toggleHidePass} >
+        <img style={{width: 20 + 'px'}} src={hidePass ? eyeOff : eye} alt="eye icon" />
+      </Button>  
+          </CustomInput>
         {errors.password && <span>{errors.password.message}</span>}
 
         <Button
