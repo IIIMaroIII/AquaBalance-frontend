074203e4c66e7463416dfc5c360cda7f5107f0ae
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-08-06 21:37:31 -0600

    upd custom hook

--- src/components/Water/WaterDetailedInfo/DailyInfo/ChooseDate/ChooseDate.jsx
+++ src/components/Water/WaterDetailedInfo/DailyInfo/ChooseDate/ChooseDate.jsx
@@ -1,30 +1,9 @@
-import { parseISO } from 'date-fns';
-import { useSelector } from 'react-redux';
-import CONSTANTS from 'src/components/Constants/constants';
-import { selectDate } from 'src/redux/water/selectors.js';
-// import css from './chooseDate.module.css';
+import useChosenDate from 'src/hooks/useChosenDate.js';
 
 const ChooseDate = () => {
-  const selectedDate = useSelector(selectDate);
+  const { checkIfToday } = useChosenDate();
 
-  const ifToday = () => {
-    const parsedDate = new Date(parseISO(selectedDate));
-    const date = new Date();
-
-    const formatedDate = date => {
-      const year = date.getFullYear();
-      const month = date.getMonth() + 1;
-      const day = date.getDate();
-
-      return `${year}/${month}/${day}`;
-    };
-    const chosenDay = CONSTANTS.DAYS[parsedDate.getDay()];
-    return formatedDate(parsedDate) === formatedDate(date)
-      ? 'Today'
-      : `${parsedDate.getDate()}, ${chosenDay}`;
-  };
-
-  return <div>{ifToday()}</div>;
+  return <div>{checkIfToday()}</div>;
 };
 
 export default ChooseDate;
