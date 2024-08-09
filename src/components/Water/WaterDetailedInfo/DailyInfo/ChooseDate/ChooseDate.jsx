b0a8cdbfa086c9ec1a9be5817be5116e181844e4
Artem0202 <artyomkov.19@gmail.com>
2024-08-09 16:14:47 +0300

    Tracker Styles

--- src/components/Water/WaterDetailedInfo/DailyInfo/ChooseDate/ChooseDate.jsx
+++ src/components/Water/WaterDetailedInfo/DailyInfo/ChooseDate/ChooseDate.jsx
@@ -1,8 +1,9 @@
 import useChosenDate from 'src/hooks/useChosenDate.js';
+import css from './chooseDate.module.css';
 
 const ChooseDate = () => {
   const { checkIfToday } = useChosenDate();
-  return <div>{checkIfToday()}</div>;
+  return <div className={css.day}>{checkIfToday()}</div>;
 };
 
 export default ChooseDate;
