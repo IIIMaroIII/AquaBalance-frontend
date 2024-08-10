5f43a747e71f625400a934ea4b4372435a2694be
Anton <antonklim6@gmail.com>
2024-08-10 12:42:42 +0200

    signin new logic

--- src/Validation/signUpFormValidation.js
+++ src/Validation/signUpFormValidation.js
@@ -2,9 +2,9 @@ import * as yup from 'yup';
 
 export const signUpFormValidation = yup.object().shape({
     email: yup
-    .string()
-    .email('Email is invalid, use format @mail.com')
-    .required('Email is required'),
+      .string()
+      .email('Email is invalid, use format name@mail.com')
+      .required('Email is required'),
     password: yup.string()
       .min(8, 'Password must be at least 8 characters')
       .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
