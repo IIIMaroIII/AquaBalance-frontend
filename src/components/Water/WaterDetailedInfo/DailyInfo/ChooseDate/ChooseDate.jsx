e3627aa945cffee338d353af3bb715c41fd88757
Nazariy111 <gerikgiga1@gmail.com>
2024-08-10 15:51:21 +0300

    fixed jsx, didn`t touch styles

--- src/components/Water/WaterDetailedInfo/DailyInfo/ChooseDate/ChooseDate.jsx
+++ src/components/Water/WaterDetailedInfo/DailyInfo/ChooseDate/ChooseDate.jsx
@@ -1,9 +1,30 @@
-import useChosenDate from 'src/hooks/useChosenDate.js';
+import { parseISO } from 'date-fns';
+import { useSelector } from 'react-redux';
+import CONSTANTS from 'src/components/Constants/constants';
+import { selectDate } from 'src/redux/water/selectors.js';
 import css from './chooseDate.module.css';
 
 const ChooseDate = () => {
-  const { checkIfToday } = useChosenDate();
-  return <div className={css.day}>{checkIfToday()}</div>;
+  const selectedDate = useSelector(selectDate);
+
+  const ifToday = () => {
+    const parsedDate = new Date(parseISO(selectedDate));
+    const date = new Date();
+
+    const formatedDate = date => {
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+
+      return `${year}/${month}/${day}`;
+    };
+    const chosenDay = CONSTANTS.DAYS[parsedDate.getDay()];
+    return formatedDate(parsedDate) === formatedDate(date)
+      ? 'Today'
+      : `${parsedDate.getDate()}, ${chosenDay}`;
+  };
+
+  return <div className={css.day}>{ifToday()}</div>;
 };
 
 export default ChooseDate;
--- src/components/Water/WaterDetailedInfo/DailyInfo/DailyInfo.jsx
+++ src/components/Water/WaterDetailedInfo/DailyInfo/DailyInfo.jsx
@@ -5,11 +5,12 @@ import css from '../DailyInfo/DailyInfo.module.css';
 import { FaPlus } from 'react-icons/fa6';
 import { changeModal, changeWaterModalAdd } from 'src/redux/water/slice.js';
 import { useDispatch } from 'react-redux';
+import Container from 'src/components/REUSABLE/Container/Container.jsx';
 
 const DailyInfo = () => {
   const dispatch = useDispatch();
   return (
-    <div className={css.dailyInfoContainer}>
+    <Container addClass={css.dailyInfoContainer}>
       <div className={css.todayLine}>
         <ChooseDate />
         <Button
@@ -19,15 +20,12 @@ const DailyInfo = () => {
             dispatch(changeModal(true));
           }}
         >
-          <span className={css.plusIcon}>
-            <FaPlus />
-          </span>
-
+          <FaPlus className={css.plusIcon} />
           <span className={css.btnSpan}>Add water</span>
         </Button>
       </div>
       <WaterList />
-    </div>
+    </Container>
   );
 };
 
