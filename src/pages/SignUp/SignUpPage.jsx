14bc1e9f7730858132403332e7005f5ccac2358d
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-08-10 16:16:41 +0300

    Add AdvantagesSection to SignUpPage for Desktop (1440px)

--- src/pages/SignUp/SignUpPage.jsx
+++ src/pages/SignUp/SignUpPage.jsx
@@ -1,18 +1,21 @@
+import { useWindowSize } from 'react-use';
+
 import SignUpForm from 'src/components/Users/SignUpForm/SignUpForm.jsx';
-// import css from './SignUpPage.module.css';
 import Container from 'src/components/REUSABLE/Container/Container';
 import Logo from 'src/components/REUSABLE/Logo/Logo';
 import { Link } from 'react-router-dom';
 import css from './SignUpPage.module.css';
-// import AdvantagesSection from 'src/components/AdvantagesSection/AdvantagesSection.jsx';
+import AdvantagesSection from 'src/components/AdvantagesSection/AdvantagesSection.jsx';
 
 const SignUpPage = () => {
   // const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
+  const { width } = useWindowSize();
+
   return (
-    <Container type="section">
+    <Container type="section" addClass={css.signUpPage}>
       <div className={css.container}>
-        <Logo />
+        <Logo addClass={css.logo} />
         <SignUpForm />
         <div className={css.haveAnAccountContainer}>
           <p className={css.haveAnAccountText}>
@@ -23,6 +26,7 @@ const SignUpPage = () => {
           </p>
         </div>
       </div>
+      {width <= 1439 ? null : <AdvantagesSection />}
       {/* тут должен быть условный рендеринг для экранов от 1440px <AdvantagesSection /> */}
     </Container>
   );
