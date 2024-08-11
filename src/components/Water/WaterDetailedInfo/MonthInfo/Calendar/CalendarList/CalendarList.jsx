50186160d1a8757204f22223e1454f7b39247f15
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-08-11 17:42:01 -0600

    upd

--- src/components/Water/WaterDetailedInfo/MonthInfo/Calendar/CalendarList/CalendarList.jsx
+++ src/components/Water/WaterDetailedInfo/MonthInfo/Calendar/CalendarList/CalendarList.jsx
@@ -2,13 +2,6 @@ import { useState } from 'react';
 import CalendarItem from './CalendarItem/CalendarItem.jsx';
 import css from './calendarList.module.css';
 import useChosenDate from 'src/hooks/useChosenDate.js';
-import { useSelector } from 'react-redux';
-import {
-  dailyNormaPercentage,
-  daysWithRecords,
-  selectMonthlyWaterItems,
-} from 'src/redux/water/selectors.js';
-import { selectUserDailyNorma } from 'src/redux/users/selectors.js';
 
 const CalendarList = () => {
   const [activeDay, setActiveDay] = useState(null);
@@ -18,21 +11,6 @@ const CalendarList = () => {
     '0',
   )}`;
 
-  // const fn = day => {
-  //   const arr = monthlyItems
-  //     .filter(item => new Date(item.date).getDate() === day)
-  //     .map(item => item.volume);
-
-  //   const total = arr.reduce((acc, num) => acc + num, 0);
-
-  //   const percent = (total / (dailyNorma * 1000)) * 100;
-
-  //   if (isNaN(percent)) {
-  //     return 0;
-  //   } else {
-  //     return Math.min(percent, 100).toFixed(0);
-  //   }
-  // };
 
 
   return (
@@ -40,7 +18,6 @@ const CalendarList = () => {
       {getDaysOfMonth().map(day => {
         return (
           <CalendarItem
-            // percentage={daysWithRecord.includes(day) ? dailyPercent(day) : 0}
             activeDay={activeDay}
             setActiveDay={setActiveDay}
             key={day}
