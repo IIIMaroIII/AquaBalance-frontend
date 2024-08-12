52a2281f040ec15389ac1ef4ee5a467e5671ec66
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-08-12 16:20:21 -0600

    fixed ProgressBar

--- src/components/Water/WaterMainInfo/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/Water/WaterMainInfo/WaterProgressBar/WaterProgressBar.jsx
@@ -1,17 +1,25 @@
+import { useRef } from 'react';
 import css from './WaterProgressBar.module.css';
 import { useSelector } from 'react-redux';
 import { dailyNormaPercentage } from 'src/redux/water/selectors.js';
 
 const WaterProgressBar = () => {
   const percentage = useSelector(dailyNormaPercentage(new Date().getDate()));
+  const ref = useRef(percentage);
+  console.log('ref :>> ', ref);
+
+  if (ref.current === 0) ref.current = percentage;
 
   return (
     <div className={css.WaterProgressBar_container}>
       <h2 className={css.WaterProgressBar_h2}>Today</h2>
       <div className={css.WaterProgressBar}>
-        <div className={css.progress} style={{ width: `${percentage}%` }}></div>
-        <div className={css.thumb} style={{ left: `${percentage}%` }}>
-          <div className={css.thumb_value}>{`${percentage}%`}</div>
+        <div
+          className={css.progress}
+          style={{ width: `${ref.current}%` }}
+        ></div>
+        <div className={css.thumb} style={{ left: `${ref.current}%` }}>
+          <div className={css.thumb_value}>{`${ref.current}%`}</div>
         </div>
       </div>
       <div className={css.progress_labels}>
