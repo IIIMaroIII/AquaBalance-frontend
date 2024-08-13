9ba3d889c8ca784a3c8553e16893285cd5462051
Paul <69437294+Escbrk@users.noreply.github.com>
2024-08-13 11:48:16 -0600

    ProgressBar fixed "if delete last record"

--- src/components/Water/WaterMainInfo/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/Water/WaterMainInfo/WaterProgressBar/WaterProgressBar.jsx
@@ -1,47 +1,63 @@
 import { useEffect, useRef, useState } from 'react';
 import css from './WaterProgressBar.module.css';
 import { useSelector } from 'react-redux';
-import { dailyNormaPercentage } from 'src/redux/water/selectors.js';
+import {
+  dailyNormaPercentage,
+  selectDate,
+  selectWaterItems,
+} from 'src/redux/water/selectors.js';
+import { ifEqualDate } from 'src/utils/ifEqualDate';
 
 const WaterProgressBar = () => {
   const percentage = useSelector(dailyNormaPercentage(new Date().getDate()));
+  const waterItemns = useSelector(selectWaterItems);
   const percent = useRef(percentage);
   const [currentPercentage, setCurrentPercentage] = useState(percent.current);
+  const chosenDate = useSelector(selectDate);
 
   useEffect(() => {
     if (percentage !== 0) {
       percent.current = percentage;
       setCurrentPercentage(percent.current);
     }
-  }, [percentage]);
-  
 
-  return (
-    <div className={css.WaterProgressBar_container}>
-      <h2 className={css.WaterProgressBar_h2}>Today</h2>
-      <div className={css.WaterProgressBar}>
-        <div
-          className={css.progress}
-          style={{
-            width: `${currentPercentage}%`,
-          }}
-        ></div>
-        <div
-          className={css.thumb}
-          style={{
-            left: `${currentPercentage}%`,
-          }}
-        >
-          <div className={css.thumb_value}>{`${currentPercentage}%`}</div>
+      if (ifEqualDate(chosenDate)) {
+        if (waterItemns.length === 0) {
+          setCurrentPercentage(0);
+        }
+      }
+  }, [chosenDate, currentPercentage, percentage, waterItemns]);
+
+
+
+
+
+    return (
+      <div className={css.WaterProgressBar_container}>
+        <h2 className={css.WaterProgressBar_h2}>Today</h2>
+        <div className={css.WaterProgressBar}>
+          <div
+            className={css.progress}
+            style={{
+              width: `${currentPercentage}%`,
+            }}
+          ></div>
+          <div
+            className={css.thumb}
+            style={{
+              left: `${currentPercentage}%`,
+            }}
+          >
+            <div className={css.thumb_value}>{`${currentPercentage}%`}</div>
+          </div>
+        </div>
+        <div className={css.progress_labels}>
+          <span>0%</span>
+          <span>50%</span>
+          <span>100%</span>
         </div>
       </div>
-      <div className={css.progress_labels}>
-        <span>0%</span>
-        <span>50%</span>
-        <span>100%</span>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default WaterProgressBar;
