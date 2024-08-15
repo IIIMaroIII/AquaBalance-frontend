9743d260469c01e7f7204eafa6f77f978bfbdd87
Paul <69437294+Escbrk@users.noreply.github.com>
2024-08-14 23:08:03 -0600

    update

--- src/components/Water/WaterDetailedInfo/DailyInfo/WaterList/WaterList.jsx
+++ src/components/Water/WaterDetailedInfo/DailyInfo/WaterList/WaterList.jsx
@@ -3,7 +3,6 @@ import css from '../WaterList/WaterList.module.css';
 import { useSelector } from 'react-redux';
 import { selectWaterItems } from 'src/redux/water/selectors.js';
 import {} from 'src/redux/water/operations.js';
-import { sortHrs } from 'src/utils/sortHrs.js';
 
 const WaterList = () => {
   const dailyItems = useSelector(selectWaterItems);
@@ -15,13 +14,11 @@ const WaterList = () => {
       ) : (
         (() => {
           const sortedDailyItems = dailyItems
-            .map(item => {
-              return { item, time: sortHrs(item.date) };
-            })
-            .sort((a, b) => a.time.localeCompare(b.time));
-            
+            .map(item => item)
+            .sort((a, b) => a.date.localeCompare(b.date));
+
           return sortedDailyItems.map(dailyItem => (
-            <WaterItem key={dailyItem.item._id} item={dailyItem.item} />
+            <WaterItem key={dailyItem._id} item={dailyItem} />
           ));
         })()
       )}
@@ -30,4 +27,3 @@ const WaterList = () => {
 };
 
 export default WaterList;
-
--- src/utils/sortHrs.js
+++
@@ -1,9 +0,0 @@
-export const sortHrs = (time) => {
-    const date = new Date(time)
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-
-    return `${hours}:${minutes}:${seconds}`
-}
-
